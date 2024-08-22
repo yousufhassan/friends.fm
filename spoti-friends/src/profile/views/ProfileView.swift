4184298ca84b26c73f768d8b385ff244c661a13e
Yousuf Hassan <yousufhassan209@gmail.com>
2024-08-21 20:48:13 -0700

    Fixed naming and small refactors

--- spoti-friends/src/profile/views/ProfileView.swift
+++ spoti-friends/src/profile/views/ProfileView.swift
@@ -3,27 +3,37 @@ import SwiftUI
 /// Renders the View for a user's profile.
 struct ProfileView: View {
     let profile: SpotifyProfile
-    @State private var recentSongs: [Track]
+    @State private var topTracks: [Track]
     @StateObject private var profileViewModel: ProfileViewModel
     @EnvironmentObject var authorizationViewModel: AuthorizationViewModel
     @EnvironmentObject var friendActivityViewModel: FriendActivityViewModel
     
-    init(profile: SpotifyProfile, recentSongs: [Track] = []) {
+    init(profile: SpotifyProfile, topTracks: [Track] = []) {
         self.profile = profile
         _profileViewModel = StateObject(wrappedValue: ProfileViewModel(user: AuthorizationViewModel().user))
-        self.recentSongs = recentSongs
+        self.topTracks = topTracks
     }
     
     var body: some View {
         GeometryReader { reader in
             ScrollView {
                 VStack {
+                    // Profile Details
                     ProfileDetails(profile: profile)
                         .environmentObject(profileViewModel)
                         .environmentObject(friendActivityViewModel)
                         .padding(.bottom, 40)
                     
-                    TrackOrArtistList(trackList: recentSongs)
+                    // Top Tracks
+                    VStack (alignment: .leading) {
+                            Text("Top Songs")
+                                .font(.body)
+                                .foregroundStyle(Color.PresetColour.whitePrimary)
+                            Text("From this month")
+                                .font(.footnote)
+                                .foregroundStyle(Color.PresetColour.whiteSecondary)
+                        TrackOrArtistList(trackList: topTracks)
+                    }
                     Spacer()
                     
                     // TODO: This needs to be moved to a view for only the logged in profile
@@ -40,7 +50,7 @@ struct ProfileView: View {
                 profileViewModel.user = authorizationViewModel.user
                 
                 Task {
-                    recentSongs = await profileViewModel.getCurrentUsersTopTracks(timeRange: .oneMonth, limit: 5) ?? []
+                    //                    topTracks = await profileViewModel.getCurrentUsersTopTracks(timeRange: .oneMonth, limit: 5) ?? []
                 }
             }
         }
@@ -68,8 +78,8 @@ struct ProfileView: View {
 
 #Preview {
     let user = UserMock.userJimHalpert
-    let recentSongs = [TrackMock.iRememberEverything, TrackMock.luxury, TrackMock.traitor]
-    ProfileView(profile: user.spotifyProfile!, recentSongs: recentSongs)
+    let topTracks = [TrackMock.iRememberEverything, TrackMock.luxury, TrackMock.traitor]
+    ProfileView(profile: user.spotifyProfile!, topTracks: topTracks)
         .environmentObject(AuthorizationViewModel())
         .environmentObject(FriendActivityViewModel(user: user, friendActivites: []))
 }
--- spoti-friends/src/profile/views/TrackOrArtistList.swift
+++ spoti-friends/src/profile/views/TrackOrArtistList.swift
@@ -9,45 +9,35 @@ import SwiftUI
 struct TrackOrArtistList: View {
     let trackList: [Track]
     var body: some View {
-        HStack {
-            VStack (alignment: .leading) {
-                Text("Recent Songs")
-                    .font(.body)
-                    .foregroundStyle(Color.PresetColour.whitePrimary)
-                
-                
-                ForEach(trackList) { track in
-                    HStack {
-                        AlbumCover(album: track.album, width: 36, height: 36, cornerRadius: 2)
+        VStack (alignment: .leading) {
+            ForEach(trackList) { track in
+                HStack {
+                    AlbumCover(album: track.album, width: 36, height: 36, cornerRadius: 2)
+                    
+                    VStack (alignment: .leading) {
+                        // Track name
+                        Text(track.name)
+                            .font(.callout)
+                            .foregroundStyle(Color.PresetColour.whitePrimary)
+                            .lineLimit(1)
                         
-                        VStack (alignment: .leading) {
-                            // Track name
-                            Text(track.name)
-                                .font(.callout)
-                                .foregroundStyle(Color.PresetColour.whitePrimary)
-                                .lineLimit(1)
-                            
-                            // Artist names
-                            HStack(spacing: 0) {
-                                let artistsArray = Array(track.artists) // Convert List<Artist> to [Artist]
-                                ForEach(artistsArray.indices, id: \.self) { index in
-                                    let artist = artistsArray[index]
-                                    
-                                    Text(index < artistsArray.count - 1
-                                         ? "\(artist.name), "
-                                         : artist.name)
-                                    .font(.footnote)
-                                    .foregroundStyle(Color.PresetColour.whiteSecondary)
-                                    
-                                }
+                        // Artist names
+                        HStack(spacing: 0) {
+                            let artistsArray = Array(track.artists) // Convert List<Artist> to [Artist]
+                            ForEach(artistsArray.indices, id: \.self) { index in
+                                let artist = artistsArray[index]
+                                
+                                Text(index < artistsArray.count - 1
+                                     ? "\(artist.name), "
+                                     : artist.name)
+                                .font(.footnote)
+                                .foregroundStyle(Color.PresetColour.whiteSecondary)
                             }
                         }
                     }
-                    .padding(.vertical, 4)
                 }
+                .padding(.vertical, 4)
             }
-            
-            Spacer()
         }
     }
 }
