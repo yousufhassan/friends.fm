2a90e2bfc4e9b0ee89f472dae64ff96c5311967f
Yousuf Hassan <yousufhassan209@gmail.com>
2024-09-12 17:25:59 -0400

    Added documentation

--- spoti-friends/src/profile/viewModels/ProfileViewModel.swift
+++ spoti-friends/src/profile/viewModels/ProfileViewModel.swift
@@ -1,19 +1,34 @@
 import Foundation
 
+/// The `ProfileViewModel` class is responsible for managing and providing data related to the `ProfileView`
+/// and subviews. It handles fetching the user's top tracks, top artists, and recent tracks, and
+/// caches this data for quicker access.
+///
+/// The class includes a cache for the top tracks and top artists, which is periodically cleared
+/// to avoid stale data. Additionally, it manages asynchronous API calls to fetch the user's
+/// follower count, public playlist count, and profile items.
+///
+/// - Parameters:
+///   - user: The `User` object representing the currently logged-in user.
+///   - topTracksCache: A dictionary that caches the user's top tracks based on the time range.
+///   - topArtistsCache: A dictionary that caches the user's top artists based on the time range.
+///   - cacheClearTimer: A timer used to automatically clear the cache every 12 hours.
+///
 class ProfileViewModel: ObservableObject {
     @Published var user: User
-    
-    // Cached values are for the logged in user only
-    private var cacheClearTimer: Timer?
-    
     @Published private var topTracksCache: [TimeRange: [Track]] = [:]
     @Published private var topArtistsCache: [TimeRange: [Artist]] = [:]
+    private var cacheClearTimer: Timer?
     
+    /// Initializes the `ProfileViewModel` with the specified `User` and starts the cache timer.
+    ///
+    /// - Parameter user: The currently logged-in `User` object.
     init(user: User){
         self.user = user
         startCacheTimer()
     }
     
+    /// Starts a timer that clears the cache every 12 hours.
     private func startCacheTimer() {
         let twelveHoursinSeconds: TimeInterval = 43200
         cacheClearTimer = Timer.scheduledTimer(withTimeInterval: twelveHoursinSeconds, repeats: true) { [weak self] _ in
@@ -21,19 +36,21 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    // Clear cached top tracks and top artusts
+    /// Clears both the top tracks and top artists caches.
     private func clearCache() {
         topTracksCache.removeAll()
         topArtistsCache.removeAll()
         printInfo("Cleared Top Tracks and Artists cache.")
     }
     
-    // Function to invalidate the timer when no longer needed
+    /// Invalidates the cache timer when the `ProfileViewModel` is deallocated.
     deinit {
         cacheClearTimer?.invalidate()
     }
     
-    /// Fetches and returns the follower count for the logged in user.
+    /// Fetches the current user's follower count from Spotify.
+    ///
+    /// - Returns: The number of followers the current user has, or `-1` if the fetch fails.
     @MainActor func getCurrentUsersFollowerCount() async -> Int {
         do {
             let accessToken = try await self.user.getSpotifyWebAccessToken().access_token
@@ -48,9 +65,11 @@ class ProfileViewModel: ObservableObject {
         return -1
     }
     
-    /// Fetches and returns the number of public playlists for the logged in user.
+    /// Fetches the current user's public playlist count from Spotify.
     ///
-    /// The endpoint only includes the user's public playlists, not their private playlists.
+    /// This count only includes public playlists.
+    ///
+    /// - Returns: The number of public playlists, or `-1` if the fetch fails.
     @MainActor func getCurrentUsersPlaylistCount() async -> Int {
         do {
             let accessToken = try await self.user.getSpotifyWebAccessToken().access_token
@@ -71,6 +90,12 @@ class ProfileViewModel: ObservableObject {
         case topArtists
     }
     
+    /// Fetches the specified profile item (recent tracks, top tracks, or top artists) based on the time range.
+    ///
+    /// - Parameters:
+    ///   - forItem: The profile item to fetch (recent tracks, top tracks, or top artists).
+    ///   - timeRange: The time range over which to calculate the data. Default is `.oneMonth`.
+    /// - Returns: A `ProfileItemsResponse?` that contains either tracks or artists.
     @MainActor func viewMoreForCurrentUser(forItem: ProfileItems, timeRange: TimeRange = .oneMonth) async -> ProfileItemsResponse? {
         let limit = 20
         
@@ -87,10 +112,10 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    /// Fetches and returns the current user's recent tracks.
+    /// Fetches the current user's recent tracks.
     ///
-    /// - Parameters:
-    ///   - limit: The maximum number of items to return. Default: 5. Minimum: 1. Maximum: 50.
+    /// - Parameter limit: The maximum number of items to return. Default: 5. Minimum: 1. Maximum: 50.
+    /// - Returns: A `TracksWithResponseMetadata?` containing the user's recent tracks.
     @MainActor func getCurrentUsersRecentTracks(limit: Int)
     async -> TracksWithResponseMetadata? {
         do {
@@ -112,11 +137,12 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
-    /// Fetches and returns the current user's top tracks.
+    /// Fetches the current user's top tracks over the specified time range.
     ///
     /// - Parameters:
-    ///   - timeRange: Over what time frame the data is calculated.
+    ///   - timeRange: The time range over which to calculate the data.
     ///   - limit: The maximum number of items to return. Default: 5. Minimum: 1. Maximum: 50.
+    /// - Returns: A `TracksWithResponseMetadata?` containing the user's top tracks.
     @MainActor func getCurrentUsersTopTracks(timeRange: TimeRange, limit: Int)
     async -> TracksWithResponseMetadata? {
         // The cache should only be used for the "View more" screen – hence the check for limit == 20.
@@ -152,19 +178,29 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
+    /// Retrieves the cached top tracks for the specified time range if they exist.
+    ///
+    /// - Parameter timeRange: The time range for the cached data.
+    /// - Returns: An optional array of `Track` objects if cached data is available.
     private func getTopTracksFromCacheIfExists(timeRange: TimeRange) -> [Track]? {
         return topTracksCache[timeRange]
     }
     
+    /// Caches the specified top tracks for the given time range.
+    ///
+    /// - Parameters:
+    ///   - topTracks: An array of `Track` objects to cache.
+    ///   - timeRange: The time range for the cached data.
     private func cacheThese(topTracks: [Track], forTimeRange timeRange: TimeRange) -> Void {
         self.topTracksCache[timeRange] = topTracks
     }
     
-    /// Fetches and returns the current user's top artists.
+    /// Fetches the current user's top artists over the specified time range.
     ///
     /// - Parameters:
-    ///   - timeRange: Over what time frame the data is calculated.
+    ///   - timeRange: The time range over which to calculate the data.
     ///   - limit: The maximum number of items to return. Default: 5. Minimum: 1. Maximum: 50.
+    /// - Returns: An `ArtistsWithResponseMetadata?` containing the user's top artists.
     @MainActor func getCurrentUsersTopArtists(timeRange: TimeRange, limit: Int)
     async -> ArtistsWithResponseMetadata? {
         // The cache should only be used for the "View more" screen – hence the check for limit == 20.
@@ -200,10 +236,19 @@ class ProfileViewModel: ObservableObject {
         }
     }
     
+    /// Retrieves the cached top artists for the specified time range if they exist.
+    ///
+    /// - Parameter timeRange: The time range for the cached data.
+    /// - Returns: An optional array of `Artist` objects if cached data is available.
     private func getTopArtistsFromCacheIfExists(timeRange: TimeRange) -> [Artist]? {
         return topArtistsCache[timeRange]
     }
     
+    /// Caches the specified top artists for the given time range.
+    ///
+    /// - Parameters:
+    ///   - topArtists: An array of `Artist` objects to cache.
+    ///   - timeRange: The time range for the cached data.
     private func cacheThese(topArtists: [Artist], forTimeRange timeRange: TimeRange) -> Void {
         self.topArtistsCache[timeRange] = topArtists
     }
@@ -257,15 +302,15 @@ extension ProfileViewModel {
         
         // These are values that are displayed on screen
         var displayLabel: String {
-                switch self {
-                case .oneMonth:
-                    return "4 weeks"
-                case .sixMonths:
-                    return "6 months"
-                case .oneYear:
-                    return "1 year"
-                }
+            switch self {
+            case .oneMonth:
+                return "4 weeks"
+            case .sixMonths:
+                return "6 months"
+            case .oneYear:
+                return "1 year"
             }
+        }
     }
     
     public class GetCurrentUserTopTracksResponse: Decodable {
