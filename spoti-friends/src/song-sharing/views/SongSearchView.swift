2a4b051c0810081c0e493f0a38c0d3f96ee3627c
Yousuf Hassan <yousufhassan209@gmail.com>
2024-12-04 01:09:10 -0800

    Dismissed keyboard when scrolling or tapping away

--- spoti-friends/src/song-sharing/views/SongSearchView.swift
+++ spoti-friends/src/song-sharing/views/SongSearchView.swift
@@ -8,28 +8,50 @@ struct SongSearchView: View {
     
     var body: some View {
         VStack {
+            // Search bar with conditional cancel button
             HStack {
                 SearchBar(placeholderText: searchBarPlaceholderText, searchText: $searchText)
                 Button(action: {
                     withAnimation(.easeOut(duration: 0.2)) {
                         self.isSearching = false
                     }
-                    
                 }) {
                     Text("Cancel")
                         .font(.footnote)
+                        .foregroundStyle(Color.PresetColour.whitePrimary)
                 }
             }
+            .padding()
             
-            Spacer()
+            Spacer() // To top-align the search bar when there are no results to show
             
+            // Search results
+            if (searchText != "") {
+                ScrollView {
+                    TrackList(tracks: [TrackMock.iRememberEverything, TrackMock.luxury, TrackMock.traitor])
+                    .padding(.horizontal)
+                }
+                .scrollDismissesKeyboard(.immediately)
+            }
         }
-        .padding()
         .searchable(text: $searchText, prompt: searchBarPlaceholderText)
-        
+        .background(
+            // Adding a clear background to capture taps to hide the keyboard
+            Color.clear
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    hideKeyboard()
+                }
+        )
     }
 }
 
+// Helper method to hide the keyboard
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
 
 #Preview {
     @Previewable @State var isSearching: Bool = true
