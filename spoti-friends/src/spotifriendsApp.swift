e18c2d0bdcd6e89225859596d50567102d4e451e
Yousuf Hassan <yousufhassan209@gmail.com>
2025-01-04 16:57:05 -0500

    Completed Mixpanel setup guide with dummy data

--- spoti-friends/src/spotifriendsApp.swift
+++ spoti-friends/src/spotifriendsApp.swift
@@ -1,8 +1,25 @@
 import SwiftUI
+import Mixpanel
 
 @main
 struct spoti_friendsApp: App {
     @StateObject private var authorizationViewModel = AuthorizationViewModel()
+    
+    init() {
+        Mixpanel.initialize(token: "0c3a07f114762c672a4cebd9ee3f1fa3", trackAutomaticEvents: false)
+        Mixpanel.mainInstance().identify(distinctId: "USER_ID")
+         
+        Mixpanel.mainInstance().people.set(properties: [
+        "$name":"Jane Doe",
+        "$email":"jane.doe@example.com",
+        "$plan":"Premium"])
+        
+        Mixpanel.mainInstance().track(event:"Sign Up", properties: [
+            "Signup Type": "Referral",
+        ])
+        
+        printInfo("Set up done")
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -14,7 +31,7 @@ struct spoti_friendsApp: App {
                         if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
                             print("App Version: \(appVersion)")
                             storeInUserDefaults(key: "appVersion", value: appVersion)
-                        }                        
+                        }
                     }
                 }
         }
