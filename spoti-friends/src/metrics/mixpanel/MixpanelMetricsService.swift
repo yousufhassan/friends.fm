019067e193b3e8185e1ea63efadab70f96cf4e3f
Yousuf Hassan <yousufhassan209@gmail.com>
2025-01-05 12:13:47 -0500

    Created DEV and PROD environments and only tracking metrics when it is PROD

--- spoti-friends/src/metrics/mixpanel/MixpanelMetricsService.swift
+++ spoti-friends/src/metrics/mixpanel/MixpanelMetricsService.swift
@@ -5,6 +5,12 @@ class MixpanelMetricsService: MetricsServiceProtocol {
     private let MIXPANEL_TOKEN = "0c3a07f114762c672a4cebd9ee3f1fa3"
     
     init() {
+#if DEV
+        printInfo("Identified DEV environment - not recording metrics.")
+        return
+#endif
+        // Ignore the IDE warning, the code will get executed in a PROD environment
+        printInfo("Identified PROD environment - recording metrics.")
         Mixpanel.initialize(token: MIXPANEL_TOKEN, trackAutomaticEvents: false)
         
         let signedInUserId = getStringFromUserDefaultsValueForKey("signedInUserId")
@@ -12,6 +18,11 @@ class MixpanelMetricsService: MetricsServiceProtocol {
     }
     
     private func track(event: MetricsEvent, properties: Properties? = nil) {
+#if DEV
+        printInfo("Recorded metric for event=\(event.rawValue) with properties=\(String(describing: properties?.debugDescription))")
+        return
+#endif
+        // Ignore the IDE warning, the code will get executed in a PROD environment
         Mixpanel.mainInstance().track(event: event.rawValue, properties: properties)
     }
     
